6148639f20e046bbe532a67e0ad611915f54a726
Erlendur H <erlendurj@valka.is>
2018-11-27 15:09:04 +0000

    Bæti við clear falli í main

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+# import info about system
+from os import system, name 
+
+# define function to clear screen 
+def clearScreen(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    # for mac and linux 
+    else: 
+        _ = system('clear') 
+
 file = open("ascii car.txt", "r")
 print(file.read())
