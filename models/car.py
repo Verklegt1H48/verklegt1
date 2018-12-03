f7bae708047640b3e6161cd9b629b424ddb7cdf2
Guðjón Björnsson <gudjonbjornsson@Gujons-MacBook-Pro.local>
2018-12-03 11:09:39 +0000

    Car utfardur en frekar

--- models/car.py
+++ models/car.py
@@ -1,5 +1,62 @@
 class Car:
-    def __init__(self,car,catagory,manufacturer,model,year,milage,seats,transmission,extras):
-        self.id = len(getCarList()) + 1
-        self.deleted = 0
-        #self.rentHistory = [][]
+    def __init__(self,id,car,category,manufacturer,model,year,milage,seats,transmission,extras):
+        #self.id = len(getCarList()) + 1
+        self.deleted = False
+        self.rentHistory = []
+    @property
+    def id(self):
+        return self.id
+    @property
+    def category(self):
+        return self.category
+    @property
+    def manufacturer(self):
+        return self.manufacturer
+    @property
+    def model(self):
+        return self.model
+    @property
+    def year(self):
+        return self.year
+    @property
+    def mileage(self):
+        return self.mileage
+    @property
+    def seats(self):
+        return self.seats
+    @property
+    def transmission(self):
+        return self.transmission
+    @property
+    def extras(self):
+        return self.extras
+    @property
+    def history(self):
+        return self.history
+    @category.setter
+    def category(self, category):
+        self.category = category
+    @manufacturer.setter
+    def manufacturer(self, manufacturer):
+        self.manufacturer = manufacturer
+    @model.setter
+    def model(self, model):
+        self.model = model
+    @year.setter
+    def year(self, year):
+        self.year = year
+    @mileage.setter
+    def mileage(self, mileage):
+        self.mileage = mileage
+    @seats.setter
+    def seats(self, seats):
+        self.seats = seats
+    @transmission.setter
+    def transmission(self, transmission):
+        self.transmission = transmission
+    @extras.setter 
+    def extras(self, extras):
+        self.extras = extras
+    @history.setter 
+    def history(self, history):
+        appendself.history = extras
