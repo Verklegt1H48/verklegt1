fd6e82545d94ac306ab6ecca8fb48a004766c077
Guðjón Björnsson <gudjonbjornsson@Gujons-MacBook-Pro.local>
2018-12-04 12:31:38 +0000

    orderrepository baett vid

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from models.car import Car
-from models.user import User
-from models.creditcard import CreditCard
+
 
 def main():
     car1 = Car()
-    user1 = User()
+
     
 if __name__ == "__main__":
     main()
