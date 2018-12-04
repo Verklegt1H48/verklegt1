dbbe2824e11049f73b24b9d90c7186dfbc8f2d01
Guðjón Björnsson <gudjonbjornsson@Gujons-MacBook-Pro.local>
2018-12-04 13:22:01 +0000

    orderrepo

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from models.car import Car
 
 
 def main():
-    car1 = Car()
-
+    #car1 = Car()
+    print('hae')
     
 if __name__ == "__main__":
     main()
--- models/order.py
+++ models/order.py
@@ -1,22 +1,32 @@
 class Order:
 
-    def __init__(self):
-        self._id = 0 #Utfaera seinna
-        self._userId = 0
-        self._carCategory = ""
-        self._carId = 0
-        self._payMethod = ""
-        self._status = False
-        self._deleted = False
+
+    #def __init__(self):
+    #    self._id = 0 #Utfaera seinna
+    #    self._userId = 0
+    #    self._carCategory = ""
+    #    self._carId = 0
+    #    self._payMethod = ""
+    #    self._status = False
+    #    self._deleted = False 
+
+    def __init__(self,id=0, userId=0, carCatagory="", carId=0, payMethod="", status=False, deleted=False):
+        self._id = id
+        self._userId = userId
+        self._carCategory = carCatagory
+        self._carId = carId
+        self._payMethod = payMethod
+        self._status = status
+        self._deleted = deleted
 
     def __str__(self):
-        "{},{},{},{},{},{}".format(self._id, self._userid, self._carCategory,\
+        "{},{},{},{},{},{}".format(self._id, self._userId, self._carCategory,\
         self._carId, self._payMethod, self._status,)
 
     def __repr__(self):
         return self.__str__()
 
-
+#Getters
     @property
     def carId(self):
         return self._carId
@@ -28,7 +38,7 @@ class Order:
     @property
     def deleted(self):
         return self._deleted
-
+#Setters
     @carId.setter
     def carId(self, value):
         self._carId = value
