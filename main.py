10363f8fe92ccb9711b8fcc76a79ecfa655b2b37
Erla Gudrun Sturludottir <erla@Erlas-MacBook-Air.local>
2018-12-06 10:22:01 +0000

    Added to gitignore

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from helpers.helpers import clearScreen
 
 def main():
     carRepo = CarRepository()
-    cars = carRepo.getCars()
+    cars = carRepo.getCarList()
     sortedCars = sorted(cars, key=attrgetter('id'))
         
 main()
