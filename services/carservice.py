02ef1d6843c742581e932c74624fca8189baaebf
Erlendur H <erlendurj@valka.is>
2018-12-08 00:09:05 +0000

    Made the repos uniform in function and form and made changes to the 'add' functions of the services

--- services/carservice.py
+++ services/carservice.py
@@ -2,6 +2,7 @@ from repositories.carrepository import CarRepository
 from operator import attrgetter, methodcaller
 from models.car import Car
 
+
 class CarService:
 
     def __init__(self):
@@ -34,18 +35,18 @@ class CarService:
     def getAndSortAvailableCars(self, attribute):
         availableCars = []
         for car in self.__cars:
-            if car.deleted == False:
+            if car.deleted is False:
                 availableCars.append(car)
         return sorted(availableCars, key=attrgetter(attribute))
 
     def getAvailableCarsByCategory(self, category):
         availableCars = []
         for car in self.__cars:
-            if car.deleted == False and car.category == category:
+            if car.deleted is False and car.category == category:
                 availableCars.append(car)
         return availableCars
 
     def getFirstAvailableCarByCategory(self, category):
         for car in self.__cars:
-            if car.deleted == False and car.category == category:
+            if car.deleted is False and car.category == category:
                 return car
