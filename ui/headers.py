7a61e3a25c30d0d685ebf82881280af286fdc3d7
Erlendur H <erlendurj@valka.is>
2018-12-10 11:40:04 +0000

    bug fixes

--- ui/headers.py
+++ ui/headers.py
@@ -27,6 +27,6 @@ mainHeader = (r"""
                        PLEASE PRESS ENTER TO CONTINUE
 -------------------------------------------------------------------------- """)
 
-carSelectHeader = ("{:10}{:10}{:7}{:15}{:10}{:7}{:10}{:8}{:15}{:10}".format("Number","Category","Price","Manufacturer",
+carSelectHeader = ("{:10}{:10}{:7}{:15}{:15}{:7}{:10}{:8}{:15}{:10}".format("Number","Category","Price","Manufacturer",
                                                                             "Model","Year", "Mileage","Seats",
                                                                             "Transmission","Extras"))
