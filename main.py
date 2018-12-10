ee790883295942c239114ce43b1e01cbff4593b2
Erlendur H <erlendurj@valka.is>
2018-12-10 10:56:21 +0000

    added a header when cars are printed

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from ui.mainui import MainUI
+from ui.headers import printHeader
 from helperfunctions.helpers import clearScreen
 from repositories.carrepository import CarRepository
 
 
 def main():
+    printHeader("main")
+    input("")
     ui = MainUI()
     ui.mainMenu()
 
