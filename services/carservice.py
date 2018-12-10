694700074b0ddb47578f42f0554f6ae6d24ade35
Erla Gudrun Sturludottir <erla@Erlas-MacBook-Air.local>
2018-12-10 01:03:04 +0000

    Added functionality to see car history

--- services/carservice.py
+++ services/carservice.py
@@ -1,13 +1,17 @@
 from repositories.carrepository import CarRepository
+from repositories.orderrepository import OrderRepository
 from operator import attrgetter, methodcaller
 from models.car import Car
+from datetime import date, timedelta, datetime
 
 
 class CarService:
 
     def __init__(self):
         self.__carRepo = CarRepository()
+        self.__orderRepo = OrderRepository()
         self.__cars = self.__carRepo.getCarList()
+        self.__orders = self.__orderRepo.getOrderList()
 
     def addCar(self):
         newCar = Car()
@@ -35,18 +39,45 @@ class CarService:
     def getAndSortAvailableCars(self, attribute):
         availableCars = []
         for car in self.__cars:
-            if car.deleted is False:
+            if car.deleted:
                 availableCars.append(car)
         return sorted(availableCars, key=attrgetter(attribute))
 
     def getAvailableCarsByCategory(self, category):
         availableCars = []
         for car in self.__cars:
-            if car.deleted is False and car.category == category:
+            if car.deleted and car.category == category:
                 availableCars.append(car)
         return availableCars
 
     def getFirstAvailableCarByCategory(self, category):
         for car in self.__cars:
-            if car.deleted is False and car.category == category:
+            if car.deleted and car.category == category:
                 return car
+    
+    def getCarOrders(self, id):
+        carhistory = []
+        for order in self.__orders:
+            if order.carId == id:
+                carhistory.append(order)
+        return carhistory
+    
+    def getCarHistory(self, id):
+        orders = self.getCarOrders(id)
+        history = []
+        for order in orders:
+            pickUpDate = order.pickUpDate
+            returnDate = order.returnDate
+            pickUpDate = datetime.strptime(pickUpDate, "%d/%m/%y")
+            returnDate = datetime.strptime(returnDate, "%d/%m/%y")
+            carHistory = returnDate - pickUpDate
+            for i in range(carHistory.days + 1):
+                date = pickUpDate + timedelta(i)
+                history.append(datetime.strftime(date, "%d/%m/%y"))
+        print(history)
+        return history
+            
+
+
+
+
