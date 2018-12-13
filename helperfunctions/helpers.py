101b8fb258b945022bc7df6de216ab8476404498
erlendur17 <erlendurj@gmail.com>
2018-12-13 15:53:33 +0000

    history function and flow

--- helperfunctions/helpers.py
+++ helperfunctions/helpers.py
@@ -21,3 +21,23 @@ def resizeWindow():
 def printList(self, list):
     for object in list:
         print(object)
+
+def getHistory(orders, id, carOrUser):
+        if carOrUser == "car":
+            form = "UserID"
+        else:
+            form = "CarID"
+        print("->Print order history for {} with ID: \"{}\"".format(carOrUser, id))
+        print("These are the history items for this {}:".format(carOrUser))
+        print("")
+        print("{:10}{:15}{:15}".format(form, "Pick Up date", "Return date"))
+        if carOrUser == "car":
+            for order in orders:
+                if str(order.carId) == str(id):
+                    print("{:10}{:15}{:15}".format(str(order.userId), order.pickUpDate, order.returnDate))
+        else:
+            for order in orders:
+                if str(order.carId) == str(id):
+                    print("{:10}{:15}{:15}".format(str(order.carId), order.pickUpDate, order.returnDate))
+        print("")
+        input("Press enter to return ")
